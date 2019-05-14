47885cdb3593ac953db1362cb0972610a0ecfba1
tsaidi74 <46447425+tsaidi74@users.noreply.github.com>
2019-05-14 15:06:37 +0200

    Update LoginController.php

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -8,6 +8,7 @@ class LoginController {
         
         $loginRepo = new LoginRepository();
         $login = $loginRepo->getLogin();
+        $_SESSION['id'] = $loginRepo->getId();
         $commentRepo = new CommentRepository();
         $comments = $commentRepo->getCommentsToValidate();
         $articleRepo = new ArticleRepository();
@@ -15,33 +16,78 @@ class LoginController {
         //var_dump ($login);
 
         if ($login == "1") {
+
         	require ('./view/admin/admin.php');
         }
-        else {
+        else { 
+        	unset($_SESSION['username']);
+        	unset($_SESSION['password']);
         	require ('./view/signin.php');
         }
 
         }
         
-	// function createLogin()  : add a ne user into the DB & if OK send back the user the homepage
+	// function createLogin()  : add a user into the DB & if OK send back the user the homepage
 
         function createLogin() {
         
         $loginRepo = new LoginRepository();
-        $login = $loginRepo->createLogin();
-        //var_dump ($login);
-        $loginRepo = new LoginRepository();
+        $loginRepo->createLogin();
         $login = $loginRepo->getLogin();
         $articleRepo = new ArticleRepository();
         $articles = $articleRepo->getArticles();
+        $commentRepo = new CommentRepository();
+        $comments = $commentRepo->getCommentsToValidate();
         //var_dump ($login);
-
-        if ($login == "1") {
-        	require ('./view/affichageAccueil.php');
+		if ($login == "1") {
+        	require ('./view/admin/admin.php');
         }
         else {
         	require ('./view/signin.php');
-        }
+        	}
 
     }
+
+        function deconnect() {
+
+       	$articleRepo = new ArticleRepository();
+        $articles = $articleRepo->getArticles();
+        
+  		if (isset($_SESSION['username']))
+
+  		{ // Si tu es connecté on te déconnecte et on te redirige vers une page.
+  
+        // Supression des variables de session
+        $_SESSION = array();
+        session_destroy();
+        //unset($_SESSION['username'])
+          
+        require ('./view/AffichageAccueil.php');
+  
+    	}
+    }
+
+    
+
+        function goAdmin() {
+
+        $loginRepo = new LoginRepository();
+        $login = $loginRepo->getLogin();
+        $commentRepo = new CommentRepository();
+        $comments = $commentRepo->getCommentsToValidate();
+        $articleRepo = new ArticleRepository();
+        $articles = $articleRepo->getAllArticles();
+        
+  		if (isset($_SESSION['username'])) {
+
+  
+        require ('./view/admin/admin.php'); 
+		}
+		else {
+         
+        require ('./view/AffichageAccueil.php');
+  
+    	}
+    }
+
 }
