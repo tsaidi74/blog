42a01fdbf4c23b0c8e40eff17d9a14a477285c1a
tsaidi74 <46447425+tsaidi74@users.noreply.github.com>
2019-05-14 14:58:23 +0200

    Update blog.php

--- view/blog.php
+++ view/blog.php
@@ -1,8 +1,13 @@
 <?php
-require ('view\header.php');
+require ('header.php');
 
 ?>
-    <div class="container" style="padding-top: 120px; text-align: center;">
+    <div class="container" style="padding-top: 120px">
+
+        <ol class="breadcrumb">
+            <li><a href="index.html">Home</a></li>
+            <li class="active">Blog</li>
+        </ol>
     <h1 >Mon Super Blog</h1>
         
         <?php
@@ -14,7 +19,7 @@ require ('view\header.php');
                         <div class="col-lg-12" style="text-align: left"> <h3><a href="index.php?page=detailpost&id=<?= HTMLSpecialChars($article['id']); ?>" > <?= HTMLSpecialChars($article['titre']); ?></a> </h3></div>
                         <div class="col-lg-12 divpad" style="text-align: left">
                             <div class="col-lg-3"><img src="../blog/assets/images/zen.png" alt="edit" width="100%"></div>
-                            <div class="col-lg-9"><?= HTMLSpecialChars($article['contenu200']); ?></div>
+                            <div class="col-lg-9"><?= HTMLSpecialChars_decode($article['contenu200']); ?></div>
                         </div>
                    
                     <div class="col-lg-12 div_titre">publié le <?= HTMLSpecialChars($article['date']); ?> par <?= HTMLSpecialChars($article['id_user']); ?></div>
