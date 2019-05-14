326032ec87e23644b4771081c02ba930cc29e2a9
tsaidi74 <46447425+tsaidi74@users.noreply.github.com>
2019-05-14 15:04:10 +0200

    Update LoginRepository.php

--- model/LoginRepository.php
+++ model/LoginRepository.php
@@ -16,20 +16,33 @@ class LoginRepository  extends Connect{
 
     }
 
+     function getId()
+    {
+    $db = $this->getDb();
+
+    $req = $db->prepare('SELECT id FROM user WHERE username = :username AND password = :password');
+    $req->execute(array(
+    'username' => $_SESSION['username'],
+    'password' => $_SESSION['password'])); 
+    $resultat = $req->fetch();
+
+    return $resultat[0];
+
+    }
+
     function createLogin()
     {
     $db = $this->getDb();
 
 
-    //$pass_hashed = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    //$pass_hashed = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    //$passed =password_hash($_SESSION['password'], PASSWORD_DEFAULT);
+   // $_SESSION['password'] =  password_hash($_POST['password'], PASSWORD_DEFAULT);
+   // var_dump($pass_hashed)
 
-    //$_SESSION['password'] = password_hash($_SESSION['password'], PASSWORD_DEFAULT);
-
-    $req = $db->prepare('INSERT INTO user (username, password, status, type) VALUES (:username, :password, "1", "1")');
+    $req = $db->prepare('INSERT INTO user (username, password) VALUES (:username, :password)');
     $req->execute(array(
     'username' => $_SESSION['username'],
     'password' => $_SESSION['password']));      
     }
 }
+
+
